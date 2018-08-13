d3ef9a83e369702173c7dfc8d00d4516455a359d
Rishi Srivastava <rishisrivasta@gmail.com>
2018-08-14 06:34:02 +0800

    added variables demo

---
+++ learning-kotlin/src/safari/primer/Variables.kt
@@ -0,0 +1,16 @@
+package safari.primer
+
+fun main(args: Array<String>) {
+/*
+    var streetNumber: Int
+    var streetName: String = "High Street"
+*/
+
+    var streetNumber = 10
+    var streetName = "High Street"
+
+    val zip = "E11 P1"
+    streetName = "Pudding Lane"
+//    zip = "SW1 ES2"
+
+}
