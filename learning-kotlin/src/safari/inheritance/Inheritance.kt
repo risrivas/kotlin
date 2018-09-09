0c6ea849c3886bb3dbb3be2431667aecb994dbc0
Rishi Srivastava <rishisrivasta@gmail.com>
2018-09-10 06:48:10 +0800

    Inheritance

---
+++ learning-kotlin/src/safari/inheritance/Inheritance.kt
@@ -0,0 +1,27 @@
+package safari.inheritance
+
+open class Person() {
+    open fun validate() {
+
+    }
+}
+
+open class Customer : Person {
+    final override fun validate() {}
+
+    constructor(): super() {}
+}
+
+class SpecialCustomer: Customer() {
+//    override fun validate() {
+//        super.validate()
+//    }
+
+}
+
+data class CustomerEntity(val name: String): Person()
+
+fun main(args: Array<String>) {
+    val customer = Customer()
+    customer.validate()
+}
