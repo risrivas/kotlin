7bb4737c7f1a72272dbb38c4a316c7300bb3ebfa
Rishi Srivastava <rishisrivasta@gmail.com>
2018-10-03 06:49:25 +0800

    lambdas

--- learning-kotlin/src/udemy/basics/lambdas.kt
+++ learning-kotlin/src/udemy/basics/lambdas.kt
@@ -1,8 +1,17 @@
 package udemy.basics
 
+fun Int.even() = this % 2 == 0
+fun Int.timesTwo() = this * 2
+fun isEven(i: Int) = i % 2 == 0
+
 fun main(args: Array<String>) {
+    // val timesTwo: (Int) -> Int = { x: Int -> x * 2 }
+    val add: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
+
     val list = (1..100).toList()
-    // println(list)
+    println(list.filter { it % 2 == 0 })
+    println(list.filter { it.even() })
+    println(list.filter(::isEven))
 
     val doubled = list.map { i -> i * 2 }
     // println(doubled)
@@ -26,8 +35,8 @@ fun main(args: Array<String>) {
     )
 
     val notFlattened = nestedList.map { it.sortedDescending() }
-    println(notFlattened)
+    // println(notFlattened)
 
     val flattened = nestedList.flatMap { it.sortedDescending() }
-    println(flattened)
+    // println(flattened)
 }
